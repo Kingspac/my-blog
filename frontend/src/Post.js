b121dc13beb52321825e73b0ebd68abe9cab2110
kingspac <kingsleybulus3@gmail.com>
2026-03-15 16:14:29 +0100

    Fix image API URL

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -5,7 +5,7 @@ export default function Post({_id,title,summary,cover,createdAt,author}){
     <div className="post">
         <div className="image">
           <Link to={`/post/${_id}`}>
-            <img src={"http://localhost:4000/" +cover} />
+            <img src={`${process.env.REACT_APP_API_URL ||"http://localhost:4000/"}+cover}`} />
           </Link>
         </div>
         <div className="text">
